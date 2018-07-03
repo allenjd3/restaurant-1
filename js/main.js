9d6de066f66caf94258c828677f295a01a711b0b
James Allen <jacques2186@yahoo.com>
2018-07-02 21:33:50 -0400

    added alt texts to the json and encorporated them into the main.js

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', restaurant.alt);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
@@ -177,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
